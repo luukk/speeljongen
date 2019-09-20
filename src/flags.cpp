28ee06ffae04fd4d65a52b6813bb86316a1c8180
luuk <klaverluuk@gmail.com>
2019-09-20 12:08:49 +0200

    flag setters

--- src/flags.cpp
+++ src/flags.cpp
@@ -3,11 +3,39 @@
 #include "registers.h"
 
 namespace cpu {
-    flags::flags(registers& registerList) {
-        reg = &registerList;
+    flags::flags(registers* registerList) {
+        reg = registerList;
     }
 
-    void flags::setCarry() {
-        reg->setF(10);
+    uint8_t flags::getCarry() {
+        // return (reg->getF() >> 4) & 0x01;
+    }
+
+    bool flags::getZero() {
+        // return((reg->getF() >> 7) & 0x01);
+    }
+
+    void flags::setCarry(bool bit) {
+        uint8_t flippedBit = transformBitPosition(bit, CARRY_BIT_POSITION);
+        reg->setF(flippedBit);
+    }
+
+    void flags::setHalfCarry(bool bit) {
+        uint8_t flippedBit = transformBitPosition(bit, HALF_CARRY_BIT_POSITION);
+        reg->setF(flippedBit);
+    }
+
+    void flags::setSubtract(bool bit) {
+        uint8_t flippedBit = transformBitPosition(bit, SUB_BIT_POSITION);
+        reg->setF(flippedBit);
+    }
+
+    void flags::setZero(bool bit) {
+        uint8_t flippedBit = transformBitPosition(bit, ZERO_BIT_POSITION);
+        reg->setF(flippedBit);
+    }
+
+    uint8_t flags::transformBitPosition(bool bitState, int bitPosition) {
+        return (reg->getF() & ~(1 << bitPosition)) | ((bitState != 0) << bitPosition);
     }
 }
--- src/flags.h
+++ src/flags.h
@@ -1,19 +1,25 @@
-#include "registers.h"
 #pragma once
+#include "registers.h"
+
+#define CARRY_BIT_POSITION 5
+#define HALF_CARRY_BIT_POSITION 6
+#define SUB_BIT_POSITION 7
+#define ZERO_BIT_POSITION 4
 
 namespace cpu {
     class flags {
         private:
             registers *reg;
+            uint8_t transformBitPosition(bool, int);
         public:
-            flags(registers& registerList);
+            flags(registers* registerList);
             uint8_t getCarry();
             uint8_t getHalfCarry();
             uint8_t getSubtract();
-            uint8_t getZero();
-            void setCarry();
-            void setHalfCarry();
-            void setSubtract();
-            void setZero();
+            bool getZero();
+            void setCarry(bool);
+            void setHalfCarry(bool);
+            void setSubtract(bool);
+            void setZero(bool);
     };
 }
